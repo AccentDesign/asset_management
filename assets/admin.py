dbda428ee89c17e44f1e3c2f4838bdfe26593a67
Stuart George <stuart.bigmassa@gmail.com>
2018-07-14 07:58:10 +0100

    add better tasks admin

--- assets/admin.py
+++ assets/admin.py
@@ -5,8 +5,37 @@ from mptt.admin import MPTTModelAdmin
 from assets.models import *
 
 
-admin.site.register(Asset, MPTTModelAdmin)
+class TaskInline(admin.StackedInline):
+    extra = 0
+    model = Task
+    readonly_fields = [
+        'last_due',
+        'next_due'
+    ]
+
+class AssetAdmin(MPTTModelAdmin):
+    inlines = [
+        TaskInline,
+    ]
+
+
+admin.site.register(Asset, AssetAdmin)
 admin.site.register(AssetType)
 admin.site.register(Manufacturer)
-admin.site.register(Task, list_display=('__str__', 'asset', 'last_due', 'next_due'))
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        '__str__',
+        'asset',
+        'last_due',
+        'next_due'
+    ]
+    readonly_fields = [
+        'last_due',
+        'next_due'
+    ]
+
+
+admin.site.register(Task, TaskAdmin)
 admin.site.register(TaskType)

bc97ad6b9cea1d0d8d364d73565ea5d84b1dd4f6
Stuart George <stuart.bigmassa@gmail.com>
2018-07-19 00:27:09 +0100

    use get_absolute_url

--- assets/models/asset_type.py
+++ assets/models/asset_type.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class AssetType(models.Model):
@@ -8,6 +9,9 @@ class AssetType(models.Model):
 
     class Meta:
         ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse_lazy('assets:asset-type-update', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.name
--- assets/models/contact.py
+++ assets/models/contact.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class Contact(models.Model):
@@ -8,6 +9,9 @@ class Contact(models.Model):
 
     class Meta:
         ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse_lazy('assets:contact-update', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.name

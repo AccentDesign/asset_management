d64c7c012adb81286b4d491e2d2fa34eeb185c34
Stuart George <stuart.bigmassa@gmail.com>
2018-07-21 00:11:13 +0100

    put home page behind login

--- app/urls.py
+++ app/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+
+from app.views import HomeView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    path('', TemplateView.as_view(template_name='home.html')),
+    path('', HomeView.as_view(), name='home'),
     path('', include('assets.urls'))
 ]
 
--- tests/test_home.py
+++ tests/test_home.py
@@ -4,5 +4,7 @@ from tests.test_case import AppTestCase
 class ViewTests(AppTestCase):
 
     def test_200_response(self):
+        user = self.create_user()
+        self.client.force_login(user)
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)

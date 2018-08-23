85b46502fc83eac9b9d44b8db940548ad1f35568
Stuart George <stuart.bigmassa@gmail.com>
2018-08-23 07:59:41 +0100

    make all edit screens stay on edit page when saved

--- assets/views/contact.py
+++ assets/views/contact.py
@@ -16,14 +16,12 @@ class ContactCreate(ActivatedTeamRequiredMixin, SuccessMessageMixin, CreateView)
     model = Contact
     form_class = ContactForm
     success_message = 'created successfully'
-    success_url = reverse_lazy('assets:contact-list')
 
 
 class ContactUpdate(ActivatedTeamRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Contact
     form_class = ContactForm
     success_message = 'updated successfully'
-    success_url = reverse_lazy('assets:contact-list')
 
 
 class ContactDelete(ActivatedTeamRequiredMixin, ProtectedDeleteMixin, DeleteSuccessMessageMixin, DeleteView):
--- assets/views/note.py
+++ assets/views/note.py
@@ -5,7 +5,6 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from app.views.mixins import ProtectedDeleteMixin, DeleteSuccessMessageMixin
 from assets.forms import NoteForm
 from assets.models import Note
-from authentication.middleware.current_user import get_current_user
 from authentication.views.mixins import ActivatedTeamRequiredMixin
 
 
@@ -17,14 +16,12 @@ class NoteCreate(ActivatedTeamRequiredMixin, SuccessMessageMixin, CreateView):
     model = Note
     form_class = NoteForm
     success_message = 'created successfully'
-    success_url = reverse_lazy('assets:note-list')
 
 
 class NoteUpdate(ActivatedTeamRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Note
     form_class = NoteForm
     success_message = 'updated successfully'
-    success_url = reverse_lazy('assets:note-list')
 
 
 class NoteDelete(ActivatedTeamRequiredMixin, ProtectedDeleteMixin, DeleteSuccessMessageMixin, DeleteView):

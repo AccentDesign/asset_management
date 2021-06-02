4754eb9f2788cff3054c5402971ad5df99684b0e
Stuart George <stuart@accentdesign.co.uk>
2021-06-02 16:32:49 +0100

    added breadcrumb to task on search results

--- assets/views/search.py
+++ assets/views/search.py
@@ -24,7 +24,8 @@ class SearchView(ActivatedTeamRequiredMixin, ListView):
         if query:
             asset_results = models.Asset.for_team.search(query)
             contact_results = models.Contact.for_team.search(query)
-            task_results = models.Task.for_team.search(query)
+            # TODO: find a way to do the asset.get_ancestors() to avoid the N+1 issue
+            task_results = models.Task.for_team.search(query).select_related('asset')
             note_results = models.Note.for_team.search(query)
             queryset_chain = chain(
                 asset_results,

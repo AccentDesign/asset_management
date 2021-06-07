88d38c25b4b517cdd578cc1432a62a6ce074bbb4
Stuart George <stuart@accentdesign.co.uk>
2021-06-07 12:06:25 +0100

    use names for tasks not emails

--- assets/forms/task.py
+++ assets/forms/task.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from app.forms.fields import UserModelChoiceField
 from app.forms.widgets import DatePicker
 from assets.models import Task, TaskHistory, TaskPriority, TaskType
 from authentication.middleware.current_user import get_current_team
@@ -51,7 +52,7 @@ class TaskListFilterForm(forms.Form):
         input_formats=('%d/%m/%Y', ),
         widget=DatePicker,
     )
-    assigned_to = forms.ModelChoiceField(
+    assigned_to = UserModelChoiceField(
         queryset=User.objects.all(),
         required=False
     )
